329a00fac406c95c0df197ada04886dfe5966f7f
Mário Rocco Pettinati <mariopettinati@gmail.com>
2014-08-14 23:45:54 -0300

    We now link to TerraLib project

--- src/library/PersistenceService.cpp
+++ src/library/PersistenceService.cpp
@@ -1,6 +1,29 @@
 #include "PersistenceService.h"
 
+#include <TeDatabase.h>
+#include <TeImportRaster.h>
+
 bool ImportImageToDatabase(TeDatabase* database, const std::string& imgFileName, const std::string& layerName)
 {
+	if(database == 0)
+	{
+		return false;
+	}
+	if(database->isConnected() == false)
+	{
+		return false;
+	}
+	
+	TeRaster* rasterIn = new TeRaster(imgFileName);
+	if(rasterIn->init() == false)
+	{
+		delete rasterIn;
+		return false;
+	}
+
+	TeLayer* layer = new TeLayer(layerName, database, rasterIn->projection());
+
+	bool result = TeImportRaster(layer, rasterIn);
+
 	return true;
 }
